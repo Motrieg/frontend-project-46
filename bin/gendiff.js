d6624b455b2cbf6ec3a8dbf08388f3640c6e7813
Михаил Савельев <motriegs@gmail.com>
2023-09-20 21:04:03 +0300

    changed gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,8 +7,6 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
-  .option('-v --version', 'output the version number')
-  .option('-h, --help', 'output usage information')
 
 program.parse();
 
