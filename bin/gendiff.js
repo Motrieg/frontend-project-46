c663ce44b5b35b26c9b99ee51af92e384ac97fc4
Михаил Савельев <motriegs@gmail.com>
2023-10-17 20:29:10 +0300

    Added options message

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,6 +7,10 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format <type>', 'output format')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
 
 program.parse();
 
